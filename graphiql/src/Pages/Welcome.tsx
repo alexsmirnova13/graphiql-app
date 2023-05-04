eb8f93f4f621318291633cbe24ce111c72bb088b
alexsmirnova13 <alexandrasmirnovae@gmail.com>
2023-05-05 00:53:20 +0300

    add: localisation

--- graphiql/src/Pages/Welcome.tsx
+++ graphiql/src/Pages/Welcome.tsx
@@ -1,5 +1,17 @@
+import { useTranslation, Trans } from 'react-i18next';
+
 const Welcome = () => {
-  return <p>Welcome</p>;
+  const { i18n } = useTranslation();
+  const changeLanguage = (lng: string) => {
+    i18n.changeLanguage(lng);
+  };
+  return (
+    <div>
+      <Trans i18nKey="welcome">Welcome to my app!</Trans>
+      <button onClick={() => changeLanguage('en')}>English</button>
+      <button onClick={() => changeLanguage('ru')}>Русский</button>
+    </div>
+  );
 };
 
 export default Welcome;
