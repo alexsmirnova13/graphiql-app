64e9d70102500a85a6f579fd32fea8b438686f66
alexsmirnova13 <alexandrasmirnovae@gmail.com>
2023-05-28 17:04:34 +0300

    feat: split chanks

--- graphiql/vite.config.ts
+++ graphiql/vite.config.ts
@@ -9,4 +9,15 @@ import svgr from 'vite-plugin-svgr';
 export default defineConfig({
   base: '',
   plugins: [react(), sassDts(), svgr()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+  },
 });
