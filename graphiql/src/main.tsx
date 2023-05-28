51a33123dba017173730dc1e0b987390a2430e29
alexsmirnova13 <alexandrasmirnovae@gmail.com>
2023-05-28 16:56:32 +0300

    fix browserRouter

--- graphiql/src/main.tsx
+++ graphiql/src/main.tsx
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './firebase.ts';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './i18n';
 import { Provider } from 'react-redux';
 import store from './store';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
